e73b5859ea13f8c54866717fac20c33af7acdab5
SMhlengi <musamhlengi@gmail.com>
2015-02-03 23:23:43 +0200

    video preview

---
+++ Revamp_LatestSightings/utils.cs
@@ -0,0 +1,382 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Configuration;
+
+namespace Revamp_LatestSightings
+{
+    public class utils
+    {
+        public static void SendSuccessfullyRegisteredMail(string firstname, string lastname, string emailAddress, string url)
+        {
+            try
+            {
+                MailMessage message = new MailMessage("No-Reply@lscms.socialengine.co.za", emailAddress);
+                SmtpClient smtpClient = new SmtpClient();
+                smtpClient.Port = 25;
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Host = "freeza.aserv.co.za";
+                NetworkCredential networkCredential = new NetworkCredential("No-Reply@socialengine.co.za", "N0-R3ply");
+                smtpClient.Credentials = (ICredentialsByHost)networkCredential;
+                message.Subject = "Successfully Registered with Latest Sightings";
+                message.IsBodyHtml = true;
+                string emailMessage = ReturnRegistrationMail(firstname, lastname, emailAddress);
+
+                message.Body = emailMessage;
+                smtpClient.Send(message);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private static string ReturnRegistrationMail(string firstname, string lastname, string emailaddress)
+        {
+            string EmailMessage = "";
+
+            EmailMessage += "<html>" + Environment.NewLine;
+            EmailMessage += "<head>" + Environment.NewLine;
+            EmailMessage += "<style>" + Environment.NewLine;
+            EmailMessage += "body { margin: 10px; }" + Environment.NewLine;
+            EmailMessage += "body { font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 12px;}" + Environment.NewLine;
+            EmailMessage += "</style>" + Environment.NewLine;
+            EmailMessage += "</head>" + Environment.NewLine;
+            EmailMessage += "<body style\"margin: 5px 0 0 0;\">" + Environment.NewLine;
+
+            // new code 
+            EmailMessage += "Howzit " + firstname + " <br /><br />";
+            EmailMessage += " Welcome to the Latest Sightings partner programme! You have successfully registered as a video contributor. We look forward to receiving your amazing wildlife videos! <br /><br />";
+            EmailMessage += "You can login to your account <a href='http://lscms2.socialengine.co.za/'> here </a> with your username ( " + emailaddress + " ). <br /><br />";
+            EmailMessage += "The way it works from here is:<br />";
+            EmailMessage += "<ol>";
+            EmailMessage += "<li> Login and upload your amazing footage <a href='http://lscms2.socialengine.co.za/'> here </a></li>";
+            EmailMessage += "<li> We'll check it out and see if we can use it.</li>";
+            EmailMessage += "<li> We'll then let you know whether it's been selected to go onto our channel. This might take some time. Once selected it might also be a while before your video goes live.</li>";
+            EmailMessage += "<li> We will update you along the way and you can always log into your account to check the status.</li>";
+            EmailMessage += "<li> At this point we will ask you to update your banking details or PayPal account.</li>";
+            EmailMessage += "<li> Once live you can sit back, relax and watch your earnings grow!</li>";
+            EmailMessage += "<li> Go on another safari and grab more amazing footage. </li> </ol><br />";
+            EmailMessage += "You can find some useful tips on shooting better videos <a href='http://vimeo.com/videoschool/101'> here. </a> <br /><br />";
+            EmailMessage += "Check out what works by going to the YouTube channel <a href='https://www.youtube.com/user/Animatedcams'> here. </a> <br /><br />";
+            EmailMessage += "Please visit us at http://www.latestsightings.com/" + ReturnEmailSignature();
+            // end of new code 
+            EmailMessage += "</body>" + Environment.NewLine;
+            EmailMessage += "</html>";
+
+            return EmailMessage;
+        }
+
+        public static bool SendForgottonPasswordEmail(string emailAddress)
+        {
+            bool mailSent = false;
+            string url = ConfigurationManager.AppSettings["forgottenPasswordUrl"].ToString();
+            url += emailAddress;
+            try
+            {
+                MailMessage message = new MailMessage("No-Reply@lscms.socialengine.co.za", emailAddress);
+                SmtpClient smtpClient = new SmtpClient();
+                smtpClient.Port = 25;
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Host = "freeza.aserv.co.za";
+                NetworkCredential networkCredential = new NetworkCredential("No-Reply@socialengine.co.za", "N0-R3ply");
+                smtpClient.Credentials = (ICredentialsByHost)networkCredential;
+                message.Subject = "Latest Sightings Reset Password";
+                message.IsBodyHtml = true;
+                string emailMessage = ReturnResetPasswordMail(url);
+
+                message.Body = emailMessage;
+                smtpClient.Send(message);
+                mailSent = true;
+            }
+            catch (Exception ex)
+            {
+            }
+            return mailSent;
+        }
+
+        private static string ReturnResetPasswordMail(string url)
+        {
+            string EmailMessage = "";
+
+            EmailMessage += "<html>" + Environment.NewLine;
+            EmailMessage += "<head>" + Environment.NewLine;
+            EmailMessage += "<style>" + Environment.NewLine;
+            EmailMessage += "body { margin: 10px; }" + Environment.NewLine;
+            EmailMessage += "body { font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 12px;}" + Environment.NewLine;
+            EmailMessage += "</style>" + Environment.NewLine;
+            EmailMessage += "</head>" + Environment.NewLine;
+            EmailMessage += "<body style\"margin: 5px 0 0 0;\">" + Environment.NewLine;
+            EmailMessage += "Howzit <br /><br />";
+            EmailMessage += "Please click on the below link to reset your password <br />" + url;
+            EmailMessage += ReturnEmailSignature();
+            EmailMessage += "</body>" + Environment.NewLine;
+            EmailMessage += "</html>";
+
+            return EmailMessage;
+        }
+
+        public static bool SendVideoEmailLinkToAdministrator(string userid, string fullname, string title, string videoId)
+        {
+            bool mailSent = false;
+            string url = ConfigurationManager.AppSettings["viewVideoUrl"].ToString();
+            url += string.Format("{0}&video={1}", userid, videoId);
+            try
+            {
+                MailMessage message = new MailMessage("No-Reply@lscms.socialengine.co.za", ConfigurationManager.AppSettings["videoAdministratorEmailAddress"].ToString());
+                SmtpClient smtpClient = new SmtpClient();
+                smtpClient.Port = 25;
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Host = "freeza.aserv.co.za";
+                NetworkCredential networkCredential = new NetworkCredential("No-Reply@socialengine.co.za", "N0-R3ply");
+                smtpClient.Credentials = (ICredentialsByHost)networkCredential;
+                message.Subject = "New Video Upload called " + title;
+
+                string str = "Howzit" + Environment.NewLine + Environment.NewLine +
+                             fullname + " has uploaded a video. " + Environment.NewLine + Environment.NewLine +
+                             "Title : " + title + Environment.NewLine + Environment.NewLine +
+                             "Please click on the below link to view / download the video." + Environment.NewLine + Environment.NewLine +
+                             url;
+
+                message.Body = str;
+                smtpClient.Send(message);
+                mailSent = true;
+            }
+            catch (Exception ex)
+            {
+            }
+            return mailSent;
+        }
+
+        public static string AddTwoStrings(string one, string two)
+        {
+            return one + " " + two;
+        }
+
+        /// <summary>
+        /// Gets the MIME type of the file name specified based on the file name's
+        /// extension.  If the file's extension is unknown, returns "octet-stream"
+        /// generic for streaming file bytes.
+        /// </summary>
+        /// <param name="sFileName">The name of the file for which the MIME type
+        /// refers to.</param>
+        public static string GetMimeTypeByFileName(string sFileName)
+        {
+            string sMime = "application/octet-stream";
+
+            string sExtension = System.IO.Path.GetExtension(sFileName);
+            if (!string.IsNullOrEmpty(sExtension))
+            {
+                sExtension = sExtension.Replace(".", "");
+                sExtension = sExtension.ToLower();
+
+                if (sExtension == "pdf")
+                {
+                    sMime = "application/pdf";
+                }
+                else if (sExtension == "webm")
+                {
+                    sMime = "video/webm";
+                }
+                else if (sExtension == "flv")
+                {
+                    sMime = "video/x-flv";
+                }
+                else if (sExtension == "ogv")
+                {
+                    sMime = "video/ogg";
+                }
+                else if (sExtension == "ogg")
+                {
+                    sMime = "video/ogg";
+                }
+                else if (sExtension == "avi")
+                {
+                    sMime = "video/x-msvideo";
+                }
+                else if (sExtension == "wmv")
+                {
+                    sMime = "video/x-ms-wmv";
+                }
+                else if (sExtension == "mp4")
+                {
+                    sMime = "video/mp4";
+                }
+                else if (sExtension == "m4v")
+                {
+                    sMime = "video/mp4";
+                }
+                else if (sExtension == "mpg")
+                {
+                    sMime = "video/mpeg";
+                }
+                else if (sExtension == "mpeg")
+                {
+                    sMime = "video/mpeg";
+                }
+                else if (sExtension == "mpe")
+                {
+                    sMime = "video/mpeg";
+                }
+                else if (sExtension == "svi")
+                {
+                    sMime = "video/x-msvideo";
+                }
+                else if (sExtension == "mov")
+                {
+                    sMime = "video/quicktime";
+                }
+            }
+
+            return sMime;
+        }
+
+        /// <summary>
+        /// Streams the bytes specified as a file with the name specified using HTTP to the 
+        /// calling browser.
+        /// </summary>
+        /// <param name="sFileName">The name of the file as it will apear when the user
+        /// clicks either open or save as in their browser to accept the file
+        /// download.</param>
+        /// <param name="fileBytes">The file as a byte array to be streamed.</param>
+        public static void StreamFileToBrowser(string sFileName, byte[] fileBytes)
+        {
+            System.Web.HttpContext context = System.Web.HttpContext.Current;
+            context.Response.Clear();
+            context.Response.ClearHeaders();
+            context.Response.ClearContent();
+            context.Response.AppendHeader("content-length", fileBytes.Length.ToString());
+            context.Response.ContentType = GetMimeTypeByFileName(sFileName);
+            context.Response.AppendHeader("content-disposition", "attachment; filename=" + sFileName);
+            context.Response.BinaryWrite(fileBytes);
+
+            // use this instead of response.end to avoid thread aborted exception (known issue):
+            // http://support.microsoft.com/kb/312629/EN-US
+            context.ApplicationInstance.CompleteRequest();
+        }
+
+        internal static void SendAcceptedVideoEmail(string useremail, string filename, string firstname)
+        {
+            try
+            {
+                MailMessage message = new MailMessage("No-Reply@lscms.socialengine.co.za", useremail);
+                SmtpClient smtpClient = new SmtpClient();
+                smtpClient.Port = 25;
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Host = "freeza.aserv.co.za";
+                NetworkCredential networkCredential = new NetworkCredential("No-Reply@socialengine.co.za", "N0-R3ply");
+                smtpClient.Credentials = (ICredentialsByHost)networkCredential;
+                message.Subject = "Latest Sightings Video Upload";
+                message.IsBodyHtml = true;
+                string emailMessage = ReturnAcceptedVideoEmail(firstname, filename);
+
+                message.Body = emailMessage;
+                smtpClient.Send(message);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private static string ReturnAcceptedVideoEmail(string firstname, string filename)
+        {
+            string EmailMessage = "";
+
+            EmailMessage += "<html>" + Environment.NewLine;
+            EmailMessage += "<head>" + Environment.NewLine;
+            EmailMessage += "<style>" + Environment.NewLine;
+            EmailMessage += "body { margin: 10px; }" + Environment.NewLine;
+            EmailMessage += "body { font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 12px;}" + Environment.NewLine;
+            EmailMessage += "</style>" + Environment.NewLine;
+            EmailMessage += "</head>" + Environment.NewLine;
+            EmailMessage += "<body style\"margin: 5px 0 0 0;\">" + Environment.NewLine;
+
+            EmailMessage += "Howzit " + firstname + " <br /><br />";
+            EmailMessage += "Great news, your video <strong>" + filename + "</strong> has been accepted. We are editing it and queueing it for publication. This might take some time.<br />Please be patient. We will let you know when it is online.";
+            EmailMessage += "<br />Please note that only once your video is online, we will ask you to log in to provide your banking details. You will also then be able to start viewing your earnings.";
+            EmailMessage += ReturnEmailSignature();
+            EmailMessage += "</body>" + Environment.NewLine;
+            EmailMessage += "</html>";
+
+            return EmailMessage;
+        }
+
+        internal static void SendDeclineEmailMessage(string useremail, string filename, string firstname, string declindedOptions, string adviceSuggestions)
+        {
+            try
+            {
+                MailMessage message = new MailMessage("No-Reply@lscms.socialengine.co.za", useremail);
+                SmtpClient smtpClient = new SmtpClient();
+                smtpClient.Port = 25;
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Host = "freeza.aserv.co.za";
+                NetworkCredential networkCredential = new NetworkCredential("No-Reply@socialengine.co.za", "N0-R3ply");
+                smtpClient.Credentials = (ICredentialsByHost)networkCredential;
+                message.Subject = "Latest Sightings Video Upload ";
+                message.IsBodyHtml = true;
+                string emailMessage = ReturnDeclineVideoMail(firstname, declindedOptions, adviceSuggestions, filename);
+
+                message.Body = emailMessage;
+                smtpClient.Send(message);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private static string ReturnDeclineVideoMail(string firstname, string declindedOptions, string adviceSuggestions, string filename)
+        {
+            string EmailMessage = "";
+
+            EmailMessage += "<html>" + Environment.NewLine;
+            EmailMessage += "<head>" + Environment.NewLine;
+            EmailMessage += "<style>" + Environment.NewLine;
+            EmailMessage += "body { margin: 10px; }" + Environment.NewLine;
+            EmailMessage += "body { font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 12px;}" + Environment.NewLine;
+            EmailMessage += "</style>" + Environment.NewLine;
+            EmailMessage += "</head>" + Environment.NewLine;
+            EmailMessage += "<body style\"margin: 5px 0 0 0;\">" + Environment.NewLine;
+
+            // new code 
+            EmailMessage += "Howzit " + firstname + " <br /><br />";
+            EmailMessage += " We regret to inform you that your video <strong> " + filename + "</strong> has been declined ";
+            if (!String.IsNullOrEmpty(declindedOptions))
+            {
+                EmailMessage += "for the following reasons. <br />";
+                EmailMessage += "<ol>";
+                string[] reasons = declindedOptions.Split(':');
+                Array.Resize(ref reasons, reasons.Length - 1);
+
+                foreach (var reason in reasons)
+                {
+                    EmailMessage += "<li>" + reason + "</li>";
+                }
+                EmailMessage += "</ol><br />";
+            }
+
+            if (!String.IsNullOrEmpty(adviceSuggestions))
+            {
+                EmailMessage += adviceSuggestions;
+            }
+            EmailMessage += "<br /><br />Should you wish to discuss this further, please get in touch with us by emailing <strong>videos@latestsightings.com.</strong><br /><br />";
+            EmailMessage += "Keep on sending in your videos.";
+            EmailMessage += ReturnEmailSignature();
+            EmailMessage += "</body>" + Environment.NewLine;
+            EmailMessage += "</html>";
+
+            return EmailMessage;
+        }
+
+        private static string ReturnEmailSignature()
+        {
+            return "<br /><br /> Warm regards <br />The Latest Sightings Team";
+        }
+    }
+}
---
+++ Revamp_LatestSightings/videopreview.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+namespace Revamp_LatestSightings
+{
+    public partial class videopreview : System.Web.UI.Page
+    {
+
+        private Boolean videoRecordFound = false;
+        protected Video videoRecord;
+        protected Dictionary<string, string> contributor;
+        public bool downloadVideoPanel = false;
+        public List<DeclineVideoStatus> declineStatuses = null;
+        public string title { get; set; }
+        public string filename { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            contributor = new Dictionary<string, string>();
+            contributor.Add("firstname", "Musa");
+            contributor.Add("lastname", "Mhlengi");
+            contributor.Add("email", "musamhlengi@gmail.com");
+            contributor.Add("cellnumber", "0847929793");
+            videoRecord = new Video();
+            videoRecord.Title = "This is just a test";
+            videoRecord.Notes = "These are just note";
+
+        }
+
+        protected void downloadVideo_Click(object sender, EventArgs e)
+        {
+            // the file name to get
+            string fileName = Hiddenfilename.Value;
+            // get the file bytes to download to the browser
+            byte[] fileBytes = System.IO.File.ReadAllBytes(ConfigurationManager.AppSettings["savePath"] + fileName);
+            // NOTE: You could also read the file bytes from a database as well.
+
+            // download this file to the browser
+            utils.StreamFileToBrowser(fileName, fileBytes);
+        }
+    }
+}
