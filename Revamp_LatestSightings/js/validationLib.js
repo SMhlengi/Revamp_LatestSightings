612e088ef78cd5b6e911c72477b0daf5afe3da91
SMhlengi <musamhlengi@gmail.com>
2015-03-22 19:28:38 +0200

    twitter work on homepage for videos

--- Revamp_LatestSightings/js/validationLib.js
+++ Revamp_LatestSightings/js/validationLib.js
@@ -501,7 +501,7 @@
         var loc = $(this).attr('href');
 
         //We get the title of the link
-        var title = encodeURIComponent("Lastest Kruger Sightings via @LatestKruger");
+        var title = encodeURIComponent($(this).parent().parent().find(".blog-grid-content").find("a").html());
 
         //We trigger a new window with the Twitter dialog, in the middle of the page
         window.open('http://twitter.com/share?url=' + youtubeVideoUrl + '&text=' + title + '&', 'twitterwindow', 'height=450, width=550, top=' + ($(window).height() / 2 - 225) + ', left=' + $(window).width() / 2 + ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
