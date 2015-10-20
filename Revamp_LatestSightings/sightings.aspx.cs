8de19b57eacca35cb6bcac29067b7a3b14816e0e
Musa <musamhlengi@gmail.com>
2015-10-21 01:34:41 +0200

    sighting page work
    early days on code behind work

--- Revamp_LatestSightings/sightings.aspx.cs
+++ Revamp_LatestSightings/sightings.aspx.cs
@@ -4,14 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using LatestSightingsLibrary;
 
 namespace Revamp_LatestSightings
 {
     public partial class sightings : System.Web.UI.Page
     {
+        string tingId { get; set; }
+        Dictionary<string, string> ting = new Dictionary<string, string>();
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            if (HasTingParameter())
+            {
+                tingId = Request.QueryString["id"].ToString();
+                ting = library.GetTingInfo(tingId);
+            }
+
+        }
+
+        private bool HasTingParameter()
+        {
+            if (!String.IsNullOrEmpty(Request.QueryString["id"]))
+                return true;
+            return false;
         }
     }
 }
