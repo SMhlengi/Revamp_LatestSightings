1c63a01296cd0569fff2cffe5eeffb0c4254157e
Musa <musamhlengi@gmail.com>
2015-12-04 09:31:45 +0200

    bug fixes
    fb twitter html changes
    homepage latest blogs fixes

--- Revamp_LatestSightings/obj/Release/Package/PackageTmp/js/homePageLatestBlogs.js
+++ Revamp_LatestSightings/obj/Release/Package/PackageTmp/js/homePageLatestBlogs.js
@@ -2,6 +2,8 @@
 
 function constructLatestBlogs(json) {
     LATESTBLOGS = json
+    console.log("---LATEST BLOGS---");
+    console.log(LATESTBLOGS);
 }
 
 function showLatestBlogs() {
@@ -12,16 +14,16 @@ function showLatestBlogs() {
 function buildBlogsHtml() {
 
     var blogList = '<div class="media recent-posts">' +
-			'<a href="/blog.aspx?id=#articleId#&cat=#categoryid#" class="pull-left"><img alt="" class="media-object sixtyBySixty" src="#articleImagePath#/#pictureurl#"></a>' +
+			'<a href="/blog/#blogurl#" class="pull-left"><img alt="" class="media-object sixtyBySixty" src="#articleImagePath#/#pictureurl#"></a>' +
 			'<div class="media-body">' +
-				'<h4 class="media-heading"><a href="/blog.aspx?id=#articleId#&cat=#categoryid#">#blogheader#</a></h4>' +
+				'<h4 class="media-heading"><a href="/blog/#blogurl#">#blogheader#</a></h4>' +
 			'</div>' +
 		'</div>';
 
     if (LATESTBLOGS.length > 0) {
         var blogshtml = "";
         for (var i = 0; i < LATESTBLOGS.length; i++) {
-            blogshtml += blogList.replace("#articleId#", LATESTBLOGS[i].id).replace("#categoryid#", LATESTBLOGS[i].categoryid).replace("#articleImagePath#", "http://cdn.socialengine.co.za/latestsightings/article").replace("#pictureurl#", LATESTBLOGS[i].picture).replace("#blogheader#", LATESTBLOGS[i].header);
+            blogshtml += blogList.replace(/#blogurl#/g, LATESTBLOGS[i].urlName.replace(/ /g, "_")).replace("#articleImagePath#", "http://cdn.socialengine.co.za/latestsightings/article").replace("#pictureurl#", LATESTBLOGS[i].picture).replace("#blogheader#", LATESTBLOGS[i].header);
         }
 
         $("#homepageLatestBlogs").prepend(blogshtml);
